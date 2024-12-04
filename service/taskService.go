160a71995260c723008ac3a42c866fd26bca14ab
john <john@virtuefun.com>
2024-12-04 14:59:51 +0800

    Redis參數使用讀取yaml檔案

--- service/taskService.go
+++ service/taskService.go
@@ -83,7 +83,6 @@ func ReciveTask(ctx *fasthttp.RequestCtx) {
 
 func AddTask(taskDetail entity.TaskDetail) {
 	//檢查鎖
-
 	if GetLock(taskDetail.Id) {
 		//golangDateTime := time.Now().Format("2006-01-02 15:04:05")
 		taskDetail.Status = "Completed"
--- utils/redis.go
+++ utils/redis.go
@@ -3,14 +3,32 @@ package utils
 import (
 	"context"
 	"github.com/redis/go-redis/v9"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
 	"log"
 )
 
+type RedisEnv struct {
+	RedisAddr     string `yaml:"redisAddr"`
+	RedisPassword string `yaml:"redisPassword"`
+	RedisDB       int    `yaml:"redisDB"`
+}
+
 func NewClient(ctx context.Context) *redis.Client {
+	file, e := ioutil.ReadFile("env.yaml")
+	if e != nil {
+		log.Fatal(e)
+	}
+	var redisEnv []RedisEnv
+	err2 := yaml.Unmarshal(file, &redisEnv)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "AnotherWine1130",
-		DB:       0,
+		Addr:     redisEnv[0].RedisAddr,
+		Password: redisEnv[0].RedisPassword,
+		DB:       redisEnv[0].RedisDB,
 	})
 
 	// Perform basic diagnostic to check if the connection is working
